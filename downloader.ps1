7a6c68b8a305476b63f5fdb5d0c7300853a1ce1a
settgamer <settgamer@gmail.com>
2025-09-09 03:15:12 -0300

    Delete downloader.ps1

--- downloader.ps1
+++
@@ -1,7 +0,0 @@
-$zipUrl = "https://github.com/settgamer/settUtility/raw/main/settUtility.zip"
-$destination = "$env:USERPROFILE\Downloads\settUtility.zip"
-
-Write-Host "Baixando settUtility.zip para a pasta Downloads..."
-Invoke-WebRequest -Uri $zipUrl -OutFile $destination -UseBasicParsing
-
-Write-Host "Download concluído: $destination"
